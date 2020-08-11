3578fa1f8cbe83cad6db0d62f66e600360a0af86
Fatemeh Mohammadi <79f.mohammadi@gmail.com>
2020-08-11 20:13:22 +0430

    Initial commit

---
+++ app/src/main/java/com/fmohammadi/whatsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fmohammadi.whatsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
