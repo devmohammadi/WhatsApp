2bfe2c317930f0054e4472c645375b3f95efe59f
Fatemeh Mohammadi <79f.mohammadi@gmail.com>
2020-08-11 20:55:23 +0430

    section 1

--- app/src/main/java/com/fmohammadi/whatsapp/MainActivity.kt
+++ app/src/main/java/com/fmohammadi/whatsapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.fmohammadi.whatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var database = FirebaseDatabase.getInstance()
+        var myRef = database.reference
+
+        myRef.setValue("Test Write To Firebase")
     }
 }
